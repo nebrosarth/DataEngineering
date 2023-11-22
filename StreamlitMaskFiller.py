767e0fe56d90ddbe7d45c6520b22e5a87c4d318b
nebrosarth <68643510+nebrosarth@users.noreply.github.com>
2023-11-22 21:23:26 +0500

    Обработка ошибок ввода

--- StreamlitMaskFiller.py
+++ StreamlitMaskFiller.py
@@ -11,8 +11,12 @@ if user_input != "":
     st.write(f"Your sentence: {user_input}")
     st.write("Here are some possible words to fill in the blank:")
     maskFiller = MaskFiller()
-    results = maskFiller.fill_mask(user_input, colorful=True)
+    try:
+        results = maskFiller.fill_mask(user_input, colorful=True)
 
-    # Pretty print the results
-    for result in results:
-        st.write(result)
+        # Pretty print the results
+        for result in results:
+            st.write(result)
+    except:
+        st.write(":red[Input error.]")
+        st.write("Example: :orange[Would you like to [MASK] with me?]")
